170613184b661f554bc968f57857ae7ae2ded713
uzair616 <ranauzair518@gmail.com>
2021-01-10 00:46:18 +0500

    last commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# User.create(username: "Uzair",password: "uzair")
+# User.create(username: "jawad",password: "jawad")
+# User.create(username: "hassan",password: "hassan")
+# User.create(username: "hussain",password: "hussain")
+# User.create(username: "ahmed",password: "ahmed")
+
+
+Message.create(body:  "First Message",user_id: 1)
+Message.create(body:  "Second Message",user_id: 2)
+Message.create(body:  "Third Message",user_id: 3)
+Message.create(body:  "Fourth Message",user_id: 4)
+Message.create(body:  "Fifth Message",user_id: 5)
